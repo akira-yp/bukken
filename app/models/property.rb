7a7af9d4ea94ef60907682ba5ba415d524fefff2
akira-yp <yog.puripuri@gmail.com>
2021-02-21 16:37:29 +0900

    テーブル間のアソシエーションとネストを設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+  validates :property_name,presence:true
+  validates :rent,presence:true
+  validates :address, presence:true
+  validates :age, presence:true
+  has_many :stations
+  accepts_nested_attributes_for :stations
 end
